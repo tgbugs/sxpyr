4b633a739454369d0f6a1187bb3e4193b14761f4
Tom Gillespie <tgbugs@gmail.com>
2022-02-04 14:51:12 -0800

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import setup
 
 def find_version(filename):
     _version_re = re.compile(r"__version__ = ['\"](.*)['\"]")
+    last = None  # match python semantics
     for line in open(filename):
         version_match = _version_re.match(line)
         if version_match:
             return version_match.group(1)
 
+    return last
 
-__version__ = find_version('sxpyr/__init__.py')
+
+__version__ = find_version('sxpyr/sxpyr.py')
 
 with open('README.org', 'rt') as f:
     long_description = f.read()
@@ -34,6 +37,14 @@ setup(name='sxpyr',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+
       ],
       keywords=('lisp reader edn clojure racket scheme '
                 'elisp emacs sexp s-expression parser parsing'),
